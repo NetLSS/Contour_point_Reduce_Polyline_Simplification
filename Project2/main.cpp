90c9443baf56305a9052f952df5115691e1d39c1
LeeSangSu <sslee@ati2000.co.kr>
2020-12-07 11:54:04 +0900

    adjust folder unit processing

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,6 +1,8 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <stdio.h>
+#include <io.h>
+#include <string>
 
 using namespace cv;
 using namespace std;
@@ -8,6 +10,9 @@ using namespace std;
 #define M_PI 3.14159265
 
 cv::Mat img = cv::imread("C:\\Users\\sangsu lee\\Desktop\\1_1274(iou_0.00).bmp", 0);
+
+
+
 
 // distance mode only-----------------------------------------
 double THRESHOLD = 0.8; 
@@ -92,117 +97,128 @@ void ReduceContourPoint(std::vector<cv::Point>& vtContour, double fDistThresh, i
 
 int main()
 {
-	
+	#pragma region file list read
+	string find_file_pattern = "D:\\2020\\DS\\Project\\2020-12-07-contour_image\\FP_cut\\*.bmp";
+	string root_path = "D:\\2020\\DS\\Project\\2020-12-07-contour_image\\FP_cut\\";
+	vector<string>::iterator ptr;
+	vector<string> files;
+	__int64 sum_original=0, sum_pass1=0, sum_pass2=0;
 
-	#pragma region 원본 라벨 영역 계산
-	Mat labels, stats, centroides;
-	int cnt = connectedComponentsWithStats(img, labels, stats, centroides);
+	struct _finddata_t fd;
+	intptr_t handle;
+	if ((handle = _findfirst(find_file_pattern.c_str(), &fd)) == -1L)
+		cout << "No file in directory!" << endl;
+	do {
+		//cout << fd.name << endl;
+		files.push_back(root_path + fd.name);
+	} while (_findnext(handle, &fd) == 0);
+	_findclose(handle);
 
-	cout << "original label" << endl;
-	for (int i = 0; i < cnt; ++i) {
-		int* p = stats.ptr<int>(i);
-		cout << "label " << i << ": " << p[4] << endl;
-	}
-	#pragma endregion
+	/*for_each(files.begin(), files.end(),
+		[](const string& n) { cout << n << endl; });*/
 
-	
+	for (ptr = files.begin(); ptr != files.end(); ++ptr) {
+		string file_name = *ptr;
+		cv::Mat img = cv::imread(file_name, 0);
 
-	//----------------------------------------------------------
-	cv::RNG rng(1332345);
+		#pragma region 원본 라벨 영역 계산
+		Mat labels, stats, centroides;
+		int cnt = connectedComponentsWithStats(img, labels, stats, centroides);
 
-	std::vector<std::vector<cv::Point>> contours;
-	std::vector<std::vector<cv::Point>>& contours_ref = contours;
-	std::vector<cv::Vec4i> hierarchy;
-	vector<int> before_size, after_size;
-
-	//findContours(img, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_TC89_L1 /*cv::CHAIN_APPROX_TC89_KCOS*/ /*cv::CHAIN_APPROX_SIMPLE*/);
-	findContours(img, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_TC89_L1);
-
-	Mat padded;
-	Mat contours_pass1 = Mat::zeros(640, 640, CV_8U);
-	img.copyTo(padded);
-	cvtColor(padded, padded, CV_GRAY2RGB);
-
-	for (size_t i = 0; i < contours.size(); i++)
-	{
-		cv::Scalar color = cv::Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-		Scalar white = Scalar(255, 255, 255);
-		drawContours(contours_pass1, contours, (int)i, white, -1, cv::LINE_8, hierarchy, 0);
-		drawContours(padded, contours, (int)i, color, 3, cv::LINE_8, hierarchy, 0);
-		before_size.push_back((int)contours[i].size());
-
-		for (int j = 0; j < contours[i].size(); ++j) {
-			circle(padded, contours[i].at(j), 1, Scalar(251, 255, 0), -1);
+		cout << "original label" << endl;
+		for (int i = 0; i < cnt; ++i) {
+			int* p = stats.ptr<int>(i);
+			cout << "label " << i << ": " << p[4] << endl;
 		}
-	}
+		#pragma endregion
 
+		//----------------------------------------------------------
+		cv::RNG rng(1332345);
 
-	std::cout << "before" << contours[0].size() << std::endl;
+		std::vector<std::vector<cv::Point>> contours;
+		std::vector<std::vector<cv::Point>>& contours_ref = contours;
+		std::vector<cv::Vec4i> hierarchy;
+		vector<int> before_size, after_size;
 
-	for (size_t i = 0; i < contours.size(); i++)
-		ReduceContourPoint(contours[i], THRESHOLD, LOOP, DEBUG_MODE);
+		//findContours(img, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_TC89_L1 /*cv::CHAIN_APPROX_TC89_KCOS*/ /*cv::CHAIN_APPROX_SIMPLE*/);
+		findContours(img, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_TC89_L1);
 
-	std::cout << "after" << contours[0].size() << std::endl;
+		Mat padded;
+		Mat contours_pass1 = Mat::zeros(640, 640, CV_8U);
+		img.copyTo(padded);
+		cvtColor(padded, padded, CV_GRAY2RGB);
 
-	imshow("pass1", contours_pass1);
-	imshow("before image", padded);
-	waitKey(50);
+		for (size_t i = 0; i < contours.size(); i++)
+		{
+			cv::Scalar color = cv::Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+			Scalar white = Scalar(255, 255, 255);
+			drawContours(contours_pass1, contours, (int)i, white, -1, cv::LINE_8, hierarchy, 0);
+			drawContours(padded, contours, (int)i, color, 3, cv::LINE_8, hierarchy, 0);
+			before_size.push_back((int)contours[i].size());
 
-	
-	#pragma region findContours 1회 후 영역 계산
-	cnt = connectedComponentsWithStats(contours_pass1, labels, stats, centroides);
-
-	cout << "pass1 label" << endl;
-	for (int i = 0; i < cnt; ++i) {
-		int* p = stats.ptr<int>(i);
-		cout << "label " << i << ": " << p[4] << endl;
-	}
-	#pragma endregion
-
-
-	cv::Mat padded_after;
-	Mat contours_pass2 = Mat::zeros(640, 640, CV_8U);
-	img.copyTo(padded_after);
-	cv::cvtColor(padded_after, padded_after, CV_GRAY2RGB);
-	for (size_t i = 0; i < contours.size(); i++)
-	{
-		cv::Scalar color = cv::Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-		Scalar white = Scalar(255, 255, 255);
-		drawContours(contours_pass2, contours, (int)i, white, -1, cv::LINE_8, hierarchy, 0);
-		drawContours(padded_after, contours, (int)i, color, 3, cv::LINE_8, hierarchy, 0);
-		after_size.push_back((int)contours[i].size());
-
-		for (int j = 0; j < contours[i].size(); ++j) {
-			circle(padded_after, contours[i].at(j), 1, Scalar(251, 255, 0), -1);
+			for (int j = 0; j < contours[i].size(); ++j) {
+				circle(padded, contours[i].at(j), 1, Scalar(251, 255, 0), -1);
+			}
 		}
-	}
-	imshow("after image", padded_after);
-	imshow("pass2", contours_pass2);
-	waitKey(50);
 
-	#pragma region findContours 2회 후 영역 계산
-	cnt = connectedComponentsWithStats(contours_pass2, labels, stats, centroides);
 
-	cout << "pass1 label" << endl;
-	for (int i = 0; i < cnt; ++i) {
-		int* p = stats.ptr<int>(i);
-		cout << "label " << i << ": " << p[4] << endl;
+		std::cout << "before" << contours[0].size() << std::endl;
+
+		for (size_t i = 0; i < contours.size(); i++)
+			ReduceContourPoint(contours[i], THRESHOLD, LOOP, DEBUG_MODE);
+
+		std::cout << "after" << contours[0].size() << std::endl;
+
+
+
+		#pragma region findContours 1회 후 영역 계산
+		cnt = connectedComponentsWithStats(contours_pass1, labels, stats, centroides);
+
+		cout << "pass1 label" << endl;
+		for (int i = 0; i < cnt; ++i) {
+			int* p = stats.ptr<int>(i);
+			cout << "label " << i << ": " << p[4] << endl;
+		}
+		#pragma endregion
+
+
+		cv::Mat padded_after;
+		Mat contours_pass2 = Mat::zeros(640, 640, CV_8U);
+		img.copyTo(padded_after);
+		cv::cvtColor(padded_after, padded_after, CV_GRAY2RGB);
+		for (size_t i = 0; i < contours.size(); i++)
+		{
+			cv::Scalar color = cv::Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+			Scalar white = Scalar(255, 255, 255);
+			drawContours(contours_pass2, contours, (int)i, white, -1, cv::LINE_8, hierarchy, 0);
+			drawContours(padded_after, contours, (int)i, color, 3, cv::LINE_8, hierarchy, 0);
+			after_size.push_back((int)contours[i].size());
+
+			for (int j = 0; j < contours[i].size(); ++j) {
+				circle(padded_after, contours[i].at(j), 1, Scalar(251, 255, 0), -1);
+			}
+		}
+
+		#pragma region findContours 2회 후 영역 계산
+		cnt = connectedComponentsWithStats(contours_pass2, labels, stats, centroides);
+
+		cout << "pass2 label" << endl;
+		for (int i = 0; i < cnt; ++i) {
+			int* p = stats.ptr<int>(i);
+			cout << "label " << i << ": " << p[4] << endl;
+		}
+		#pragma endregion
+
+
+		assert(after_size.size() == before_size.size());
+		for (int i = 0; i < after_size.size(); ++i) {
+			cout << "before : " << before_size[i] << "\tafter : " << after_size[i] << endl;
+		}
+
 	}
+
 	#pragma endregion
 
-	cout << "ang_min: " << ang_min << endl;
-	cout << "ang_max: " << ang_max << endl;
-
-	std::cout << "LOOP: " << LOOP << std::endl;
-	std::cout << "THRESHOLD_ANGLE: " << THRESHOLD_ANGLE << std::endl;
-	std::cout << "THRESHOLD_DISTANCE: " << DELETE_MAX_DISTANCE << std::endl;
-	std::cout << "FORCE_DELETE_DISTANCE: " << FORCE_DELETE_DISTANCE << std::endl;
-	std::cout << "result" << std::endl;
-	assert(after_size.size() == before_size.size());
-	for (int i = 0; i < after_size.size(); ++i) {
-		cout << "before : " << before_size[i] << "\tafter : " << after_size[i] << endl;
-	}
-	
 	while (true) {
 		waitKey(50);
 	}
