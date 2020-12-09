b2a1e2105ba053002376f9b47a4744937d8b98b8
LeeSangSu <sslee@ati2000.co.kr>
2020-12-09 10:29:11 +0900

    replace distance method add csv_result3

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -14,10 +14,8 @@ using namespace std;
 cv::Mat img = cv::imread("C:\\Users\\sangsu lee\\Desktop\\1_1274(iou_0.00).bmp", 0);
 
 
-
-
 // distance mode only-----------------------------------------
-double THRESHOLD = 0.8; 
+double THRESHOLD = 0.9; 
 //------------------------------------------------------------
 
 // angle mode only -------------------------------------------
@@ -82,7 +80,7 @@ void ReduceContourPoint(std::vector<cv::Point>& vtContour, double fDistThresh, i
 			if (isDbug) cout << pt2 << endl;
 			if (isDbug) cout << pt3 << endl;
 
-			double pt2_angle = get_angle(pt1, pt2, pt3, DEBUG_MODE);
+			/*double pt2_angle = get_angle(pt1, pt2, pt3, DEBUG_MODE);
 			double distance_pt1_pt2 = distance_point(pt1, pt2);
 			if (isDbug) cout << "angle: " << pt2_angle << endl;
 			if (isDbug) cout << "distance_pt1_pt3: " << distance_pt1_pt2 << endl;
@@ -93,7 +91,34 @@ void ReduceContourPoint(std::vector<cv::Point>& vtContour, double fDistThresh, i
 			}
 			else {
 				it++;
+			}*/
+
+			#pragma region distance mode
+			double fDistance;
+			double area, AB;
+			double distance_pt1_pt2 = distance_point(pt1, pt2);
+
+			if (pt1.x == pt3.x) fDistance = abs(pt1.x - pt2.x);
+			else if (pt1.y == pt3.y) fDistance = abs(pt1.y - pt2.y);
+			else
+			{
+				area = abs((pt1.x - pt2.x) * (pt3.y - pt2.y) - (pt1.y - pt2.y) * (pt3.x - pt2.x));
+				AB = pow(pow(pt1.x - pt3.x, 2) + pow(pt1.y - pt3.y, 2), 0.5);
+				fDistance = (area / AB);
 			}
+
+			if (isDbug) cout << count << " distance : " << fDistance << endl;
+			//cout << "distance" << endl;
+			if (fDistance < THRESHOLD || distance_pt1_pt2 <= FORCE_DELETE_DISTANCE)
+			{
+				//컨투어벡터에서 pt2에 해당하는것 제거
+				//remove_pt.push_back(pt2);
+				vtContour.erase(std::remove(vtContour.begin(), vtContour.end(), pt2), vtContour.end());
+			}
+			else {
+				it++;
+			}
+			#pragma endregion
 		}
 
 	}
@@ -102,12 +127,16 @@ void ReduceContourPoint(std::vector<cv::Point>& vtContour, double fDistThresh, i
 
 int main()
 {
-	ofstream csv_result, csv_result2;
+	ofstream csv_result, csv_result2, csv_result3;
+
 	csv_result.open("csv_result.csv");
 	csv_result2.open("csv_result2.csv");
+	csv_result3.open("csv_result_capacity.csv");
+	
 	csv_result << "file,index,before_pt,after_pt,before_area,after_area" << endl;
 	csv_result2 << "file,index,before_pt,after_pt,before_area,after_area" << endl;
-
+	csv_result3 << "file,index,original_capa,reduced_capa" << endl;
+	
 	#pragma region file list read
 	string find_file_pattern = "D:\\2020\\DS\\Project\\2020-12-07-contour_image\\FP_cut\\*.bmp";
 	string root_path = "D:\\2020\\DS\\Project\\2020-12-07-contour_image\\FP_cut\\";
@@ -125,6 +154,8 @@ int main()
 		files.push_back(root_path + fd.name); // 특정 폴더 내 파일 목록 불러오기
 	} while (_findnext(handle, &fd) == 0);
 	_findclose(handle);
+	#pragma endregion
+
 
 	cout << files.size() << endl;
 	int fnc_cnt = 0;
@@ -148,13 +179,14 @@ int main()
 		//----------------------------------------------------------
 		cv::RNG rng(1332345);
 
-		std::vector<std::vector<cv::Point>> contours;
-		std::vector<std::vector<cv::Point>>& contours_ref = contours;
-		std::vector<cv::Vec4i> hierarchy;
+		vector<vector<cv::Point>> contours, contours_APPROX_SIMPLE;
+		//vector<vector<cv::Point>>& contours_ref = contours;
+		vector<cv::Vec4i> hierarchy, hierarchy_APPROX_SIMPLE;
 		vector<int> before_size, after_size;
 
 		//findContours(img, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_TC89_L1 /*cv::CHAIN_APPROX_TC89_KCOS*/ /*cv::CHAIN_APPROX_SIMPLE*/);
 		findContours(img, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_TC89_L1);
+		findContours(img, contours_APPROX_SIMPLE, hierarchy_APPROX_SIMPLE, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
 
 		Mat padded;
 		Mat contours_pass1 = Mat::zeros(640, 640, CV_8U);
@@ -168,7 +200,9 @@ int main()
 			drawContours(contours_pass1, contours, (int)i, white, -1, cv::LINE_8, hierarchy, 0);
 			//drawContours(padded, contours, (int)i, color, 3, cv::LINE_8, hierarchy, 0);
 			before_size.push_back((int)contours[i].size());
-			sum_pt_before += (int)contours[i].size();
+			
+			//sum_pt_before += (int)contours[i].size();
+			sum_pt_before += (int)contours_APPROX_SIMPLE[i].size();
 
 			/*for (int j = 0; j < contours[i].size(); ++j) {
 				circle(padded, contours[i].at(j), 1, Scalar(251, 255, 0), -1);
@@ -182,8 +216,6 @@ int main()
 			ReduceContourPoint(contours[i], THRESHOLD, LOOP, DEBUG_MODE);
 
 		if (DEBUG_MODE) std::cout << "after" << contours[0].size() << std::endl;
-
-
 
 		#pragma region findContours 1회 후 영역 계산
 		cnt = connectedComponentsWithStats(contours_pass1, labels, stats, centroides);
@@ -218,7 +250,7 @@ int main()
 			}*/
 		}
 
-		#pragma region findContours 2회 후 영역 계산
+		#pragma region 2회(reduce) 후 영역 계산
 		cnt = connectedComponentsWithStats(contours_pass2, labels, stats, centroides);
 
 		vector<int> after_area(contours.size());
@@ -250,6 +282,7 @@ int main()
 			percent = (1.0 * abs(sum_pass1 - sum_pass2) / sum_pass1) * 100.0;
 
 		csv_result2 << format("%s,%d,%d,%d,%d,%d,%f\n", file_name.c_str(), k, sum_pt_before, sum_pt_after, sum_pass1, sum_pass2, percent);
+		csv_result3 << format("%s,%d,%d,%d\n", file_name.c_str(), k, sum_pt_before * 8, sum_pt_after * 8);
 		cout << fnc_cnt <<","<<percent<<"%"<< endl;
 
 		sum_original = 0;
@@ -257,16 +290,16 @@ int main()
 		sum_pass2 = 0;
 		sum_pt_after = 0;
 		sum_pt_before = 0;
-		
 	}
-
-	#pragma endregion
 
 	/*cout << sum_original << endl;
 	cout << sum_pass1 << endl;
 	cout << sum_pass2 << endl;*/
+
 	csv_result.close();
 	csv_result2.close();
+	csv_result3.close();
+	
 	while (true) {
 		waitKey(50);
 	}
